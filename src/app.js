c253dc818ef5d5d6e0a9d6a8be51817a03d4b940
priyanshu mehta <mehtapriyanshu1028@gmail.com>
2025-09-12 21:37:33 +0530

    booking logic razorpay test integration

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import inquiryRoutes from "./routes/inquiry.routes.js";
 import getDashboardSummary from "./routes/admin.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
 import PackageGroup from "./routes/packageGroup.routes.js";
+import OrderRoutes from "./routes/booking.routes.js";
 
 dotenv.config();
 
@@ -34,6 +35,7 @@ app.use("/api/inquiries", inquiryRoutes);
 app.use("/api/admin", getDashboardSummary);
 app.use("/api/review",reviewRoutes);
 app.use("/api/packageGroup",PackageGroup);
+app.use("/api/order",OrderRoutes)
 
 app.get("/", (req, res) => {
   res.send("Tour & Travel API is running...");
